c3362894e147cdf2de39494271d8cbcbd2fe98af
Harshith <compvrar@gmail.com>
2018-10-19 15:15:33 +0530

    commit 2

---
+++ VR-pac-man-R-U/Assets/ClydeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClydeController : MonoBehaviour {
+
+    public Transform Clyde;
+
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject pos3;
+    public GameObject pos4;
+    public GameObject pos5;
+    public GameObject pos6;
+
+    public int num;
+    public float step;
+    // Use this for initialization
+    void Start () {
+        step = 3 * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("TimeCheck");
+        //num = Random.Range(1, 7);
+
+            if (num == 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos1.transform.position, step);
+            }
+            if (num == 2)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos2.transform.position, 0.4f);
+            }
+            if (num == 3)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos3.transform.position, 0.4f);
+            }
+            if (num == 4)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos4.transform.position, 0.4f);
+            }
+            if (num == 5)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos5.transform.position, 0.4f);
+            }
+            if (num == 6)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos6.transform.position, 0.4f);
+            } 
+    }
+
+    IEnumerator TimeCheck()
+    {
+        yield return new WaitForSeconds(3);
+        
+    }
+}
